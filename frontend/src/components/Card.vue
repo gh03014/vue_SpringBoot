73f8451c1c769bc8a898cc1d2a2604fbbcdebc8d
gh03014 <jaehwan3399@gmail.com>
2024-03-17 18:25:43 +0900

    Vue.js와 SpringBoot

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card shadow-sm">
+    <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}"/>
+    <div class="card-body">
+      <p class="card-text">{{item.name}}</p>
+      <div class="d-flex justify-content-between align-items-center">
+        <button class="btn btn-primary">구입하기</button>
+        <small class="text-body-secondary">
+            {{item.price}}원
+        </small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style scoped>
+.card .img {
+  display: inline-block;
+  width: 100%;
+  height: 250px;
+  background-size: cover;
+  background-position: center;
+}
+</style>
